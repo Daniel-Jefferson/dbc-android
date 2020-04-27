135acee3de1b86155a0e145ee25feff923cf5ea7
Daniel Jefferson <jeffersondaniel046@gmail.com>
2020-04-27 14:44:01 -0700

    complete update home screen

--- app/src/main/java/com/app/budsbank/adapters/VoucherAdapter.java
+++ app/src/main/java/com/app/budsbank/adapters/VoucherAdapter.java
@@ -1,6 +1,7 @@
 package com.app.budsbank.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.budsbank.R;
+import com.app.budsbank.interfaces.Communicator;
 import com.app.budsbank.models.VoucherModel;
 import com.app.budsbank.utils.AppConstants;
 
@@ -23,9 +25,11 @@ public class VoucherAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     private Context mContext;
     private ArrayList<VoucherModel> data;
     private String isFrom;
+    private Communicator communicator;
 
-    public VoucherAdapter(Context context, ArrayList<VoucherModel> data, String isFrom) {
+    public VoucherAdapter(Context context, Communicator communicator, ArrayList<VoucherModel> data, String isFrom) {
         this.mContext = context;
+        this.communicator = communicator;
         inflater = LayoutInflater.from(context);
         this.data = data;
         this.isFrom = isFrom;
@@ -54,7 +58,7 @@ public class VoucherAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         mViewHolder.tvAddress.setText(model.getDispensaryAddress());
         if(isFrom!=null && !TextUtils.isEmpty(isFrom)) {
             if(isFrom.equals(AppConstants.IsFrom.DISPENSARY_FRAGMENT.getValue())) {
-                mViewHolder.tvRedeem.setText(R.string.learn_more);
+                mViewHolder.tvRedeem.setText(R.string.redeem);
             } else if(isFrom.equals(AppConstants.IsFrom.QUIZ_FRAGMENT.getValue())) {
                 mViewHolder.tvRedeem.setText(R.string.start_quiz);
             } else if(isFrom.equals(AppConstants.IsFrom.HOME_FRAGMENT.getValue())) {
@@ -65,7 +69,7 @@ public class VoucherAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         mViewHolder.tvRedeem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                communicator.switchTab(0);
             }
         });
         mViewHolder.ivArrow.setOnClickListener(new View.OnClickListener() {
